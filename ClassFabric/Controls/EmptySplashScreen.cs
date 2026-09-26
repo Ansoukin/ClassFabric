ClassIsland/ClassIsland

perf: 修复各种流泄露的问题

ClassIsland/Controls/EmptySplashScreen.cs
@@ -11,14 +11,33 @@ namespace ClassIsland.Controls;
 
 public class EmptySplashScreen : IFAApplicationSplashScreen
 {
+    private static readonly Lazy<Bitmap> AppIconBitmap = new(() =>
+    {
+        using var stream = AssetLoader.Open(new Uri("avares://ClassIsland/Assets/AppLogo@384w.png"));
+        return new Bitmap(stream);
+    });
+
+    static EmptySplashScreen()
+    {
+        static void DisposeIcon()
+        {
+            if (AppIconBitmap.IsValueCreated)
+            {
+                AppIconBitmap.Value.Dispose();
+            }
+        }
+
+        AppDomain.CurrentDomain.ProcessExit += (_, _) => DisposeIcon();
+        AppDomain.CurrentDomain.DomainUnload += (_, _) => DisposeIcon();
+    }
+
     public async Task RunTasks(CancellationToken cancellationToken)
     {
     }
 
     public string AppName { get; } = "ClassIsland";
 
-    public IImage AppIcon { get; } =
-        new Bitmap(AssetLoader.Open(new Uri("avares://ClassIsland/Assets/AppLogo@384w.png")));
+    public IImage AppIcon { get; } = AppIconBitmap.Value;
     public object? SplashScreenContent { get; } = null;
     public int MinimumShowTime { get; } = 0;
 }

ClassIsland/Controls/ProfileTransferProviders/CsesImportProvider.cs
@@ -12,6 +12,7 @@ using ClassIsland.Core;
 using ClassIsland.Core.Abstractions.Controls;
 using ClassIsland.Core.Abstractions.Controls.ProfileTransferProviders;
 using ClassIsland.Core.Abstractions.Services;
+using ClassIsland.Core.Extensions;
 using ClassIsland.Core.Helpers.UI;
 using ClassIsland.Platforms.Abstraction;
 using ClassIsland.Services;
@@ -51,8 +52,8 @@ public partial class CsesImportProvider : GenericImportProviderBase
             await using var stream = await file.OpenReadAsync();
             using var reader = new StreamReader(stream);
             var csesProfile = CsesLoader.LoadFromYamlString(await reader.ReadToEndAsync());
-            var templateProfileJson =
-                await new StreamReader(AssetLoader.Open(new Uri("avares://ClassIsland/Assets/default-subjects.json"))).ReadToEndAsync();
+            var templateProfileJson = await AssetLoader.ReadAllTextAsync(
+                new Uri("avares://ClassIsland/Assets/default-subjects.json"));
             var templateProfile = JsonSerializer.Deserialize<Profile>(templateProfileJson);
             var profile = csesProfile.ToClassIslandObject(ImportType == 0 ? ProfileService.Profile : templateProfile);
             if (ImportType == 1)

ClassIsland/Helpers/ProfileTransferHelpers/ClassWidgets2ProfileTransferHelper.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using Avalonia.Platform;
+using ClassIsland.Core.Extensions;
 using ClassIsland.Models.External.ClassWidgets;
 using ClassIsland.Shared.Helpers;
 using ClassIsland.Shared.Models.Profile;
@@ -28,8 +29,8 @@ internal static class ClassWidgets2ProfileTransferHelper
     {
         if (profile == null)
         {
-            using var reader = new StreamReader(AssetLoader.Open(new Uri("avares://ClassIsland/Assets/default-subjects.json")));
-            profile = JsonSerializer.Deserialize<Profile>(reader.ReadToEnd())
+            var templateJson = AssetLoader.ReadAllText(new Uri("avares://ClassIsland/Assets/default-subjects.json"));
+            profile = JsonSerializer.Deserialize<Profile>(templateJson)
                       ?? throw new InvalidDataException("无法加载默认档案模板。");
         }
 

ClassIsland/Services/ConsoleService.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Windows;
 using Avalonia.Platform;
+using ClassIsland.Core.Extensions;
 using Pastel;
 
 namespace ClassIsland.Services;
@@ -17,8 +18,7 @@ public class ConsoleService
 
     public static void PrintAppInfo()
     {
-        var s = AssetLoader.Open(new Uri("avares://ClassIsland/Assets/AsciiLogo.txt"));
-        AsciiLogo = new StreamReader(s).ReadToEnd();
+        AsciiLogo = AssetLoader.ReadAllText(new Uri("avares://ClassIsland/Assets/AsciiLogo.txt"));
         Console.WriteLine(AsciiLogo.Pastel("#00bfff"));
         Console.WriteLine($"ClassIsland {App.AppVersionLong}");
         Console.WriteLine("「赐你，众星俱焚的曙光！」".Pastel("#F4EF74"));

ClassIsland/Services/ProfileService.cs
@@ -9,6 +9,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using Avalonia.Platform;
+using ClassIsland.Core.Extensions;
 using ClassIsland.Core;
 using ClassIsland.Core.Abstractions.Services;
 using ClassIsland.Core.Abstractions.Services.Management;
@@ -148,7 +149,8 @@ public class ProfileService : IProfileService, INotifyPropertyChanged
             Logger.LogInformation("档案不存在：{}", path);
             if (!ManagementService.IsManagementEnabled)  // 在集控模式下不需要默认科目
             {
-                var subject = new StreamReader(AssetLoader.Open(new Uri("avares://ClassIsland/Assets/default-subjects.json"))).ReadToEnd();
+                var subject = await AssetLoader.ReadAllTextAsync(
+                    new Uri("avares://ClassIsland/Assets/default-subjects.json"));
                 Profile.Subjects = JsonSerializer.Deserialize<Profile>(subject)!.Subjects;
             }
             SaveProfile(filename);

ClassIsland/Views/SettingsWindowNew.axaml.cs
@@ -11,6 +11,7 @@ using System.Windows;
 using ClassIsland.Core;
 using ClassIsland.Core.Abstractions.Controls;
 using ClassIsland.Core.Extensions.UI;
+using ClassIsland.Core.Extensions;
 using ClassIsland.Core.Abstractions.Services;
 using ClassIsland.Core.Abstractions.Services.Management;
 using ClassIsland.Core.Attributes;
@@ -278,9 +279,7 @@ public partial class SettingsWindowNew : ViewBase, IFANavigationPageFactory
 
         if (ViewModel.EchoCaveTextsAll.Count <= 0)
         {
-            var stream = AssetLoader.Open(new Uri("avares://ClassIsland/Assets/Tellings.txt"));
-
-            var sayings = await new StreamReader(stream).ReadToEndAsync();
+            var sayings = await AssetLoader.ReadAllTextAsync(new Uri("avares://ClassIsland/Assets/Tellings.txt"));
             if (_resourcesReleased)
             {
                 return;

ClassIsland/Views/WelcomePages/LicensePage.axaml.cs
@@ -6,6 +6,7 @@ using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Platform;
 using ClassIsland.Core.Controls;
+using ClassIsland.Core.Extensions;
 using ClassIsland.Core.Extensions.UI;
 using FluentAvalonia.UI.Controls;
 
@@ -20,8 +21,7 @@ public partial class LicensePage : UserControl
 
     private async void ButtonShowOssLicense_OnClick(object? sender, RoutedEventArgs e)
     {
-        var license = await new StreamReader(AssetLoader.Open(new Uri("avares://ClassIsland/Assets/LICENSE.txt")))
-            .ReadToEndAsync();
+        var license = await AssetLoader.ReadAllTextAsync(new Uri("avares://ClassIsland/Assets/LICENSE.txt"));
         await new FAContentDialog()
         {
             Title = "开放源代码许可",

Ansoukin/ClassFabric

ClassFabric/Controls/EmptySplashScreen.cs
@@ -11,14 +11,33 @@ namespace ClassIsland.Controls;
 
 public class EmptySplashScreen : IFAApplicationSplashScreen
 {
+    private static readonly Lazy<Bitmap> AppIconBitmap = new(() =>
+    {
+        using var stream = AssetLoader.Open(new Uri("avares://ClassFabric/Assets/AppLogo@384w.png"));
+        return new Bitmap(stream);
+    });
+
+    static EmptySplashScreen()
+    {
+        static void DisposeIcon()
+        {
+            if (AppIconBitmap.IsValueCreated)
+            {
+                AppIconBitmap.Value.Dispose();
+            }
+        }
+
+        AppDomain.CurrentDomain.ProcessExit += (_, _) => DisposeIcon();
+        AppDomain.CurrentDomain.DomainUnload += (_, _) => DisposeIcon();
+    }
+
     public async Task RunTasks(CancellationToken cancellationToken)
     {
     }
 
     public string AppName { get; } = "ClassFabric";
 
-    public IImage AppIcon { get; } =
-        new Bitmap(AssetLoader.Open(new Uri("avares://ClassFabric/Assets/AppLogo@384w.png")));
+public IImage AppIcon { get; } = AppIconBitmap.Value;
     public object? SplashScreenContent { get; } = null;
     public int MinimumShowTime { get; } = 0;
 }

ClassFabric/Controls/ProfileTransferProviders/CsesImportProvider.cs
@@ -12,6 +12,7 @@ using ClassIsland.Core;
 using ClassIsland.Core.Abstractions.Controls;
 using ClassIsland.Core.Abstractions.Controls.ProfileTransferProviders;
 using ClassIsland.Core.Abstractions.Services;
+using ClassIsland.Core.Extensions;
 using ClassIsland.Core.Helpers.UI;
 using ClassIsland.Platforms.Abstraction;
 using ClassIsland.Services;
@@ -51,8 +52,8 @@ public partial class CsesImportProvider : GenericImportProviderBase
             await using var stream = await file.OpenReadAsync();
             using var reader = new StreamReader(stream);
             var csesProfile = CsesLoader.LoadFromYamlString(await reader.ReadToEndAsync());
-            var templateProfileJson =
-                await new StreamReader(AssetLoader.Open(new Uri("avares://ClassFabric/Assets/default-subjects.json"))).ReadToEndAsync();
+            var templateProfileJson = await AssetLoader.ReadAllTextAsync(
+                new Uri("avares://ClassFabric/Assets/default-subjects.json"));
             var templateProfile = JsonSerializer.Deserialize<Profile>(templateProfileJson);
             var profile = csesProfile.ToClassFabricObject(ImportType == 0 ? ProfileService.Profile : templateProfile);
             if (ImportType == 1)

ClassFabric/Helpers/ProfileTransferHelpers/ClassWidgets2ProfileTransferHelper.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using Avalonia.Platform;
+using ClassIsland.Core.Extensions;
 using ClassIsland.Models.External.ClassWidgets;
 using ClassIsland.Shared.Helpers;
 using ClassIsland.Shared.Models.Profile;
@@ -28,8 +29,8 @@ internal static class ClassWidgets2ProfileTransferHelper
     {
         if (profile == null)
         {
-            using var reader = new StreamReader(AssetLoader.Open(new Uri("avares://ClassFabric/Assets/default-subjects.json")));
-            profile = JsonSerializer.Deserialize<Profile>(reader.ReadToEnd())
+            var templateJson = AssetLoader.ReadAllText(new Uri("avares://ClassFabric/Assets/default-subjects.json"));
+            profile = JsonSerializer.Deserialize<Profile>(templateJson)
                       ?? throw new InvalidDataException("无法加载默认档案模板。");
         }
 

ClassFabric/Services/ConsoleService.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Windows;
 using Avalonia.Platform;
+using ClassIsland.Core.Extensions;
 using Pastel;
 
 namespace ClassIsland.Services;
@@ -17,8 +18,7 @@ public class ConsoleService
 
     public static void PrintAppInfo()
     {
-        var s = AssetLoader.Open(new Uri("avares://ClassFabric/Assets/AsciiLogo.txt"));
-        AsciiLogo = new StreamReader(s).ReadToEnd();
+        AsciiLogo = AssetLoader.ReadAllText(new Uri("avares://ClassFabric/Assets/AsciiLogo.txt"));
         Console.WriteLine(AsciiLogo.Pastel("#00bfff"));
         Console.WriteLine($"ClassFabric {App.AppVersionLong}");
         Console.WriteLine("「赐你，众星俱焚的曙光！」".Pastel("#F4EF74"));

ClassFabric/Services/ProfileService.cs
@@ -9,6 +9,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using Avalonia.Platform;
+using ClassIsland.Core.Extensions;
 using ClassIsland.Core;
 using ClassIsland.Core.Abstractions.Services;
 using ClassIsland.Core.Abstractions.Services.Management;
@@ -148,7 +149,8 @@ public class ProfileService : IProfileService, INotifyPropertyChanged
             Logger.LogInformation("档案不存在：{}", path);
             if (!ManagementService.IsManagementEnabled)  // 在集控模式下不需要默认科目
             {
-                var subject = new StreamReader(AssetLoader.Open(new Uri("avares://ClassFabric/Assets/default-subjects.json"))).ReadToEnd();
+var subject = await AssetLoader.ReadAllTextAsync(
+                    new Uri("avares://ClassFabric/Assets/default-subjects.json"));
                 Profile.Subjects = JsonSerializer.Deserialize<Profile>(subject)!.Subjects;
             }
             SaveProfile(filename);

ClassFabric/Views/SettingsWindowNew.axaml.cs
@@ -11,6 +11,7 @@ using System.Windows;
 using ClassIsland.Core;
 using ClassIsland.Core.Abstractions.Controls;
 using ClassIsland.Core.Extensions.UI;
+using ClassIsland.Core.Extensions;
 using ClassIsland.Core.Abstractions.Services;
 using ClassIsland.Core.Abstractions.Services.Management;
 using ClassIsland.Core.Attributes;
@@ -278,9 +279,7 @@ public partial class SettingsWindowNew : ViewBase, IFANavigationPageFactory
 
         if (ViewModel.EchoCaveTextsAll.Count <= 0)
         {
-            var stream = AssetLoader.Open(new Uri("avares://ClassFabric/Assets/Tellings.txt"));
-
-            var sayings = await new StreamReader(stream).ReadToEndAsync();
+            var sayings = await AssetLoader.ReadAllTextAsync(new Uri("avares://ClassFabric/Assets/Tellings.txt"));
             if (_resourcesReleased)
             {
                 return;

ClassFabric/Views/WelcomePages/LicensePage.axaml.cs
@@ -6,6 +6,7 @@ using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Platform;
 using ClassIsland.Core.Controls;
+using ClassIsland.Core.Extensions;
 using ClassIsland.Core.Extensions.UI;
 using FluentAvalonia.UI.Controls;
 
@@ -20,8 +21,7 @@ public partial class LicensePage : UserControl
 
     private async void ButtonShowOssLicense_OnClick(object? sender, RoutedEventArgs e)
     {
-        var license = await new StreamReader(AssetLoader.Open(new Uri("avares://ClassFabric/Assets/LICENSE.txt")))
-            .ReadToEndAsync();
+var license = await AssetLoader.ReadAllTextAsync(new Uri("avares://ClassFabric/Assets/LICENSE.txt"));
         await new FAContentDialog()
         {
             Title = "开放源代码许可",